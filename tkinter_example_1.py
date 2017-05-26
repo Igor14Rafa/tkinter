7e5737cf185bb6e914ebaed9fe6cbad11350e9d4
Igor Rafael <yggor14rafa@hotmail.com>
2017-05-26 15:15:24 -0300

    Updating example #1

--- tkinter_example_1.py
+++ tkinter_example_1.py
@@ -4,87 +4,87 @@ class TkFileDialogExample(Tkinter.Frame):
     """This example shows how to ask user to enter a file, saving it's absolute path. Based on a Windows System.  
     """
 
-  def __init__(self, root):
+    def __init__(self, root):
 
-    Tkinter.Frame.__init__(self, root)
+        Tkinter.Frame.__init__(self, root)
 
-    # options for buttons
-    button_opt = {'fill': Tkconstants.BOTH, 'padx': 5, 'pady': 5}
+        # options for buttons
+        button_opt = {'fill': Tkconstants.BOTH, 'padx': 5, 'pady': 5}
 
-    # define buttons
-    Tkinter.Button(self, text='askopenfile', command=self.askopenfile).pack(**button_opt)
-    Tkinter.Button(self, text='askopenfilename', command=self.askopenfilename).pack(**button_opt)
-    Tkinter.Button(self, text='asksaveasfile', command=self.asksaveasfile).pack(**button_opt)
-    Tkinter.Button(self, text='asksaveasfilename', command=self.asksaveasfilename).pack(**button_opt)
-    Tkinter.Button(self, text='askdirectory', command=self.askdirectory).pack(**button_opt)
+        # define buttons
+        Tkinter.Button(self, text='askopenfile', command=self.askopenfile).pack(**button_opt)
+        Tkinter.Button(self, text='askopenfilename', command=self.askopenfilename).pack(**button_opt)
+        Tkinter.Button(self, text='asksaveasfile', command=self.asksaveasfile).pack(**button_opt)
+        Tkinter.Button(self, text='asksaveasfilename', command=self.asksaveasfilename).pack(**button_opt)
+        Tkinter.Button(self, text='askdirectory', command=self.askdirectory).pack(**button_opt)
 
-    # define options for opening or saving a file
-    self.file_opt = options = {}
-    options['defaultextension'] = '.txt'
-    options['filetypes'] = [('all files', '.*'), ('text files', '.txt')]
-    options['initialdir'] = 'C:\\'
-    options['initialfile'] = 'myfile.txt'
-    options['parent'] = root
-    options['title'] = 'This is a title'
+        # define options for opening or saving a file
+        self.file_opt = options = {}
+        options['defaultextension'] = '.txt'
+        options['filetypes'] = [('all files', '.*'), ('text files', '.txt')]
+        options['initialdir'] = 'C:\\'
+        options['initialfile'] = 'myfile.txt'
+        options['parent'] = root
+        options['title'] = 'Choose a file'
 
-    # This is only available on the Macintosh, and only when Navigation Services are installed.
-    #options['message'] = 'message'
+        # This is only available on the Macintosh, and only when Navigation Services are installed.
+        #options['message'] = 'message'
 
-    # if you use the multiple file version of the module functions this option is set automatically.
-    #options['multiple'] = 1
+        # if you use the multiple file version of the module functions this option is set automatically.
+        #options['multiple'] = 1
 
-    # defining options for opening a directory
-    self.dir_opt = options = {}
-    options['initialdir'] = 'C:\\'
-    options['mustexist'] = False
-    options['parent'] = root
-    options['title'] = 'This is a title'
+        # defining options for opening a directory
+        self.dir_opt = options = {}
+        options['initialdir'] = 'C:\\'
+        options['mustexist'] = False
+        options['parent'] = root
+        options['title'] = 'Choose a directory'
 
-  def askopenfile(self):
+    def askopenfile(self):
 
-    """Returns an opened file in read mode."""
+        """Returns an opened file in read mode."""
 
-    return tkFileDialog.askopenfile(mode='r', **self.file_opt)
+        return tkFileDialog.askopenfile(mode='r', **self.file_opt)
 
-  def askopenfilename(self):
+    def askopenfilename(self):
 
-    """Returns an opened file in read mode.
-    This time the dialog just returns a filename and the file is opened by your own code.
-    """
+        """Returns an opened file in read mode.
+        This time the dialog just returns a filename and the file is opened by your own code.
+        """
 
-    # get filename
-    filename = tkFileDialog.askopenfilename(**self.file_opt)
+        # get filename
+        filename = tkFileDialog.askopenfilename(**self.file_opt)
 
-    # open file on your own
-    if filename:
-      return open(filename, 'r')
+        # open file on your own
+        if filename:
+          return open(filename, 'r')
 
-  def asksaveasfile(self):
+    def asksaveasfile(self):
 
-    """Returns an opened file in write mode."""
+        """Returns an opened file in write mode."""
 
-    return tkFileDialog.asksaveasfile(mode='w', **self.file_opt)
+        return tkFileDialog.asksaveasfile(mode='w', **self.file_opt)
 
-  def asksaveasfilename(self):
+    def asksaveasfilename(self):
 
-    """Returns an opened file in write mode.
-    This time the dialog just returns a filename and the file is opened by your own code.
-    """
+        """Returns an opened file in write mode.
+        This time the dialog just returns a filename and the file is opened by your own code.
+        """
 
-    # get filename
-    filename = tkFileDialog.asksaveasfilename(**self.file_opt)
+        # get filename
+        filename = tkFileDialog.asksaveasfilename(**self.file_opt)
 
-    # open file on your own
-    if filename:
-      return open(filename, 'w')
+        # open file on your own
+        if filename:
+          return open(filename, 'w')
 
-  def askdirectory(self):
+    def askdirectory(self):
 
-    """Returns a selected directoryname."""
+        """Returns a selected directoryname."""
 
-    return tkFileDialog.askdirectory(**self.dir_opt)
+        return tkFileDialog.askdirectory(**self.dir_opt)
 
 if __name__=='__main__':
-  root = Tkinter.Tk()
-  TkFileDialogExample(root).pack()
-  root.mainloop()
+    root = Tkinter.Tk()
+    TkFileDialogExample(root).pack()
+    root.mainloop()
